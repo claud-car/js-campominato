b5f8e23d93a07bd0ad5a608a83bd8afbafd1820c
Claudio <claudiocarchietti@yahoo.it>
2021-03-09 18:58:36 +0100

    Esercizio terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,42 @@
 //variabili
 var numeromax = 100;
 var numeripc = [];
+var numeriutente = []
+var numeriRichiesti;
+
+//ciclo per generare 16 numeri pc
+while (numeripc.length < 16) {
+  var numero = generator(numeromax);
+  if (!numeripc.includes(numero)){
+    numeripc.push(numero);
+  }
+}
+
+
+while (numeriutente.length < numeromax - 16 && !numeripc.includes(numeriRichiesti)){
+  numeriRichiesti = parseInt(prompt("Dimmi un numero"));
+  if (numeripc.includes(numeriRichiesti)) {
+  } else if (isNaN(numeriRichiesti)){
+    console.log("Dammi un numero");
+  } else if (numeriRichiesti > 100 || numeriRichiesti < 1){
+    console.log("dammi un numero da 1 a 100");
+  } else{
+    numeriutente.push(numeriRichiesti);
+  }
+}
+console.log(numeriutente);
+
+var punteggio = numeriutente.length;
+console.log("Hai perso. Il tuo punteggio è : " + punteggio);
+
+
+
+
+
+
 
 
 //funzione per generare numeri da 1 a 100
 function generator(numeromax){
   return Math.floor(Math.random() * numeromax) + 1;
 }
-
-//ciclo per generare 16 numeri pc
-
-// while (numeripc.length < 16) {
-//   numeripc.push(generator(numeromax))
-// }
-
-
-while (numeripc.lenght < 16) {
-  var numero = generator(numeromax)
-  if (!numeripc.includes(numero)){
-    numeripc.push(numero);
-  }
-}
-
-console.log(numeripc);
