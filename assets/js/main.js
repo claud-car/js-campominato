2c6b895fae7df749f37befaa07a3d6d316af35a2
Claudio <claudiocarchietti@yahoo.it>
2021-03-10 16:23:23 +0100

    Aggiunto bonus all'esercizio,e riformulazione del codice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,31 @@
 //variabili
-var numeromax = 100;
+var numeromax = [];
 var numeripc = [];
 var numeriutente = []
 var numeriRichiesti;
+
+
+selezioneDifficolta = true;
+while (selezioneDifficolta) {
+  var difficolta = prompt("Scegli un livello di difficoltà : facile , medio , difficile");
+
+  selezioneDifficolta = false;
+  switch (difficolta) {
+    case "facile":
+      var numeromax = 100;
+      break;
+    case "medio":
+      var numeromax = 80;
+      break;
+    case "difficile":
+      var numeromax = 50;
+      break;
+    default:
+      selezioneDifficolta = true;
+      console.log("Mi devi selezionare una difficoltà da facile medio o difficile");
+      break;
+  }
+}
 
 //ciclo per generare 16 numeri pc
 while (numeripc.length < 16) {
@@ -12,8 +35,9 @@ while (numeripc.length < 16) {
   }
 }
 
+//l'utente deve scegliere dei numeri per non sbagliare
 while (numeriutente.length < numeromax - 16 && !numeripc.includes(numeriRichiesti)){
-  numeriRichiesti = parseInt(prompt("Dimmi un numero"));
+  numeriRichiesti = parseInt(prompt("Dammi un numero da 1 a 100 e prova a battere il computer"));
   if (numeripc.includes(numeriRichiesti)) {
   } else if (isNaN(numeriRichiesti)){
     console.log("Dammi un numero");
@@ -24,7 +48,9 @@ while (numeriutente.length < numeromax - 16 && !numeripc.includes(numeriRichiest
   }
 }
 
+//alla fine del gioco invio il punteggio in log
 var punteggio = numeriutente.length;
 console.log("Numeri pc : " + numeripc);
 console.log("Numeri utente indovinati: " + numeriutente);
-console.log("Hai perso. Il tuo punteggio è : " + punteggio);
+console.log("GAME OVER");
+console.log("Il tuo punteggio è : " + punteggio);
